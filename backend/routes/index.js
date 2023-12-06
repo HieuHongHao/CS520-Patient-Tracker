6608b0d1acf38cad34d86d0d6f1ebdf4e4d8ddcf
ThomasN12 <phuocthinhdn12023@gmail.com>
2023-12-05 20:56:21 -0500

    add patient router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import MedicalHistoryRouter from './medicalHistory.js';
+import DoctorRouter from './doctor.js';
+import PatientRouter from './user.js';
 const router = Router();
 
 // Add routers from each model here.
 router.use('/medicalHistory', MedicalHistoryRouter);
-
+router.use('/doctor', DoctorRouter);
+router.use('/user', PatientRouter);
 export default router;
