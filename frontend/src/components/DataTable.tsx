eed012dafa7634b3cc0f9b970242e873991f6d02
theanh28 <atvu@umass.edu>
2023-12-14 18:49:15 -0500

    hotfix DataTable lack params in PatientPage

--- frontend/src/components/DataTable.tsx
+++ frontend/src/components/DataTable.tsx
@@ -24,8 +24,8 @@ interface DataTableProps<TData, TValue> {
 export function DataTable<TData, TValue>({
   columns,
   data,
-  rowSelection,
-  setRowSelection
+  rowSelection = {},
+  setRowSelection = () => {}
 }: DataTableProps<TData, TValue>) {
   
   
